01e65d39a0537266d98eee7758778f5bd15ad8b1
Pantera <rafael.leal@totvs.com.br>
2019-07-18 15:25:20 -0300

    Added more tests for the listing router

--- internal/pkg/http/rest/listing.go
+++ internal/pkg/http/rest/listing.go
@@ -1,11 +1,22 @@
 package rest
 
 import (
+	"encoding/json"
+	"errors"
 	"net/http"
+	"strconv"
 
 	"github.com/julienschmidt/httprouter"
 	"github.com/rafaeldias/mobilitee-back-end-developer/internal/pkg/listing/device"
 )
+
+var errInvalidID = errors.New(`id parameter must be an integer and greater than 0`)
+
+// Err is the struct that will be returned as
+// the responste to a failed HTTP request
+type Err struct {
+	Error string
+}
 
 // HTTPGetter is an interface that handles the datails of
 // parsing an GET request to the server
@@ -13,10 +24,61 @@ type HTTPGetter interface {
 	GET(path string, h httprouter.Handle)
 }
 
+// Devices is the struct that will be returned as
+// the response to a succesful HTTP request for listing
+// all devices
+type Devices struct {
+	Data []*device.Device
+}
+
 // GetDevices handles the requests for reading devices
 func GetDevices(router HTTPGetter, r device.Reader) {
-	router.GET("/devices", func(rw http.ResponseWriter, req *http.Request, p httprouter.Params) {
+	router.GET("/devices", getDevicesHandler(r))
+	router.GET("/devices/:id", getDevicesHandler(r))
+}
+
+func getDevicesHandler(r device.Reader) httprouter.Handle {
+	return func(rw http.ResponseWriter, req *http.Request, p httprouter.Params) {
+		var (
+			id      int
+			output  interface{}
+			encoder = json.NewEncoder(rw)
+		)
+
+		rw.Header().Set("Content-Type", "application/json")
+
+		if param := p.ByName("id"); param != "" {
+			pid, err := strconv.Atoi(param)
+			if err != nil || pid == 0 {
+				rw.WriteHeader(http.StatusBadRequest)
+				encoder.Encode(Err{errInvalidID.Error()})
+				return
+			}
+
+			id = pid
+		}
+
+		devices, err := r.Read(id)
+		if err != nil {
+			rw.WriteHeader(http.StatusInternalServerError)
+			encoder.Encode(Err{err.Error()})
+			return
+		}
+
+		if id > 0 && len(devices) == 0 {
+			rw.WriteHeader(http.StatusNotFound)
+			encoder.Encode(Err{http.StatusText(http.StatusNotFound)})
+			return
+		}
+
 		rw.WriteHeader(http.StatusOK)
-	})
-	router.GET("/devices/:id", nil)
+
+		if id > 0 {
+			output = devices[0]
+		} else {
+			output = Devices{devices}
+		}
+
+		encoder.Encode(output)
+	}
 }
--- internal/pkg/http/rest/listing_test.go
+++ internal/pkg/http/rest/listing_test.go
@@ -1,9 +1,13 @@
 package rest
 
 import (
-	"testing"
-	"net/http/httptest"
+	"encoding/json"
+	"errors"
 	"net/http"
+	"net/http/httptest"
+	"reflect"
+	"strconv"
+	"testing"
 
 	"github.com/julienschmidt/httprouter"
 	"github.com/rafaeldias/mobilitee-back-end-developer/internal/pkg/listing/device"
@@ -22,33 +26,6 @@ func (g *getterTest) GET(path string, h httprouter.Handle) {
 	g.handle[path] = h
 }
 
-
-func TestGetDeviceRoutes(t *testing.T) {
-	testCases := []struct {
-		path string
-	}{
-		{"/devices"},
-		{"/devices/:id"},
-	}
-
-	for _, tc := range testCases {
-		getter := &getterTest{}
-		GetDevices(getter, nil)
-
-		var pathInList bool
-
-		for _, path := range getter.paths {
-			if pathInList = path == tc.path; pathInList {
-				break
-			}
-		}
-
-		if !pathInList {
-			t.Errorf("got path `%s` out of list, want it in", tc.path)
-		}
-	}
-}
-
 type deviceReader struct {
 	id      int
 	err     error
@@ -58,22 +35,65 @@ type deviceReader struct {
 func (r *deviceReader) Read(id int) ([]*device.Device, error) {
 	r.id = id
 	return r.devices, r.err
-
 }
 
-func TestGetDevicesHandle(t *testing.T) {
+func TestGetDevices(t *testing.T) {
 	testCases := []struct {
-		path           string
-		params         httprouter.Params
-		getter         *getterTest
-		reader         *deviceReader
-		wantStatusCode int
+		path            string
+		params          httprouter.Params
+		getter          *getterTest
+		reader          *deviceReader
+		wantDevices     *Devices
+		wantDevice      *device.Device
+		wantContentType string
+		wantStatusCode  int
 	}{
 		{
 			"/devices",
 			httprouter.Params{},
 			&getterTest{},
-			&deviceReader{},
+			&deviceReader{
+				devices: []*device.Device{
+					{ID: 1, Name: "Testing"},
+				},
+			},
+			&Devices{
+				Data: []*device.Device{
+					{ID: 1, Name: "Testing"},
+				},
+			},
+			nil,
+			"application/json",
+			http.StatusOK,
+		},
+		{
+			"/devices",
+			httprouter.Params{},
+			&getterTest{},
+			&deviceReader{
+				devices: []*device.Device{},
+			},
+			&Devices{
+				Data: []*device.Device{},
+			},
+			nil,
+			"application/json",
+			http.StatusOK,
+		},
+		{
+			"/devices/:id",
+			httprouter.Params{
+				httprouter.Param{"id", "1"},
+			},
+			&getterTest{},
+			&deviceReader{
+				devices: []*device.Device{
+					{ID: 1, Name: "Testing"},
+				},
+			},
+			nil,
+			&device.Device{ID: 1, Name: "Testing"},
+			"application/json",
 			http.StatusOK,
 		},
 	}
@@ -86,9 +106,145 @@ func TestGetDevicesHandle(t *testing.T) {
 
 		tc.getter.handle[tc.path](rw, req, tc.params)
 
-		r := rw.Result()
+		res := rw.Result()
 
-		if r.StatusCode != tc.wantStatusCode {
-			t.Errorf("got http status code %d, want %d", r.StatusCode, tc.wantStatusCode)
+		if res.StatusCode != tc.wantStatusCode {
+			t.Errorf("got http status code %d, want %d", res.StatusCode, tc.wantStatusCode)
+		}
+
+		if c := rw.Header().Get("Content-Type"); c != tc.wantContentType {
+			t.Errorf("got http header `Content-Type`: %s, want %s", c, tc.wantContentType)
+		}
+
+		// Test for GET /devices response
+		if tc.wantDevices != nil {
+
+			if tc.reader.id != 0 {
+				t.Errorf("got Read() param: %d, want 0", tc.reader.id)
+			}
+
+			var devices *Devices
+
+			if err := json.NewDecoder(res.Body).Decode(&devices); err != nil {
+				t.Errorf("got error while decoding response body: %s, want nil", err.Error())
+			}
+
+			if !reflect.DeepEqual(tc.wantDevices, devices) {
+				t.Errorf("got response body: %+v, want %+v", devices, tc.wantDevices)
+			}
+		}
+
+		// Test for GET /devices/:id response
+		if tc.wantDevice != nil {
+			p := tc.params.ByName("id")
+			id, _ := strconv.Atoi(p)
+
+			if id != tc.reader.id {
+				t.Errorf("got Read() param: %d, want %d", id, tc.reader.id)
+			}
+
+			var device *device.Device
+
+			if err := json.NewDecoder(res.Body).Decode(&device); err != nil {
+				t.Errorf("got error while decoding response body: %s, want nil", err.Error())
+			}
+
+			if !reflect.DeepEqual(tc.wantDevice, device) {
+				t.Errorf("got response body: %+v, want %+v", device, tc.wantDevice)
+			}
 		}
 	}
+}
+
+func TestGetDevicesError(t *testing.T) {
+	testCases := []struct {
+		path            string
+		params          httprouter.Params
+		getter          *getterTest
+		reader          *deviceReader
+		wantError       Err
+		wantContentType string
+		wantStatusCode  int
+	}{
+		{
+			"/devices",
+			httprouter.Params{},
+			&getterTest{},
+			&deviceReader{
+				err: errors.New("Testing Error"),
+			},
+			Err{"Testing Error"},
+			"application/json",
+			http.StatusInternalServerError,
+		},
+		{
+			"/devices/:id",
+			httprouter.Params{
+				httprouter.Param{"id", "1"},
+			},
+			&getterTest{},
+			&deviceReader{
+				devices: []*device.Device{},
+			},
+			Err{http.StatusText(http.StatusNotFound)},
+			"application/json",
+			http.StatusNotFound,
+		},
+		{
+			"/devices/:id",
+			httprouter.Params{
+				httprouter.Param{"id", "xzy"},
+			},
+			&getterTest{},
+			&deviceReader{
+				devices: []*device.Device{},
+			},
+			Err{errInvalidID.Error()},
+			"application/json",
+			http.StatusBadRequest,
+		},
+		{
+			"/devices/:id",
+			httprouter.Params{
+				httprouter.Param{"id", "0"},
+			},
+			&getterTest{},
+			&deviceReader{
+				devices: []*device.Device{},
+			},
+			Err{errInvalidID.Error()},
+			"application/json",
+			http.StatusBadRequest,
+		},
+	}
+
+	for _, tc := range testCases {
+		GetDevices(tc.getter, tc.reader)
+
+		rw := httptest.NewRecorder()
+		req := httptest.NewRequest(http.MethodGet, tc.path, nil)
+
+		tc.getter.handle[tc.path](rw, req, tc.params)
+
+		res := rw.Result()
+
+		if res.StatusCode != tc.wantStatusCode {
+			t.Errorf("got http status code %d, want %d", res.StatusCode, tc.wantStatusCode)
+		}
+
+		if c := rw.Header().Get("Content-Type"); c != tc.wantContentType {
+			t.Errorf("got http header `Content-Type`: %s, want %s", c, tc.wantContentType)
+		}
+
+		var e Err
+
+		if err := json.NewDecoder(res.Body).Decode(&e); err != nil {
+			t.Errorf("got error while decoding response body: %s, want nil", err.Error())
+		}
+
+		if !reflect.DeepEqual(tc.wantError, e) {
+			t.Errorf("got response body: %+v, want %+v", e, tc.wantError)
+		}
+
+	}
+}
