b9aa24058f2e11a4858b7b7121263e66ebb56ca2
rafaeldias <rafaeldias.c@gmail.com>
2019-07-19 00:29:50 -0300

    Testing Heroku setup

---
+++ cmd/api/heroku/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"os"
+	"net/http"
+)
+
+func main() string {
+	port := os.Getenv("PORT")
+
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+			w.Write([]byte("Hello"))
+	})
+
+	if err := http.ListenAndServer(post, nil); err != nil {
+		panic(err)
+	}
+}
