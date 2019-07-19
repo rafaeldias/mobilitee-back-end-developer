153b3e165637b993154614202f3d966334eb6984
rafaeldias <rafaeldias.c@gmail.com>
2019-07-19 00:33:25 -0300

    Fixed typo on main.go

--- cmd/api/heroku/main.go
+++ cmd/api/heroku/main.go
@@ -9,7 +9,7 @@ func main() string {
 	port := os.Getenv("PORT")
 
 	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-			w.Write([]byte("Hello"))
+			rw.Write([]byte("Hello"))
 	})
 
 	if err := http.ListenAndServe(post, nil); err != nil {
